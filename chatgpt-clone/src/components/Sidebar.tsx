7f6aa2ad8efc6a944d22d423257ce381f3338130
guilhermeo97 <55219644+guilhermeo97@users.noreply.github.com>
2023-06-28 19:32:06 -0300

    init
    ....

---
+++ chatgpt-clone/src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+type Props = {
+    children: ReactNode;
+}
+
+export const Sidebar = ({children }: Props) => {
+    return(
+        <section className="">
+
+        </section>
+    )
+}
